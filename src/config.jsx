b237eca4af963fce8622d23454351facff78d38b
Nicholas Major <nick@nmajor.com>
2016-02-25 06:19:40 +0700

    Another adjustment to the api endpoint

--- src/config.jsx
+++ src/config.jsx
@@ -5,6 +5,6 @@ module.exports = function() {
     // return 'http://dockerhost/'
     return 'http://localhost:3000/'
   } else {
-    return 'http://api.dathobby.com';
+    return 'http://api.dathobby.com/';
   }
 }();
